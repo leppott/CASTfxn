cf27fed1c14f9c12a5d458ff9a84ac031707d15b
leppott <Erik.Leppo@tetratech.com>
2018-06-06 12:36:59 -0400

    v0.1.0.9013
    Add CASTfxn.R so the package has a help file.

---
+++ R/CASTfxn.R
@@ -0,0 +1,29 @@
+#' @title CASTfxn: functions for Causal Assessment Screening Tool (CAST)
+#'
+#' @description Custom functions for CAST.
+#' 
+#' @section CASTfxn functions:
+#' 
+#' CoOccur
+#' 
+#' getAlgStressorResponses
+#' 
+#' getBMImatches
+#' 
+#' getBMIStressorResponses
+#' 
+#' getChemDataSubsets
+#' 
+#' getClusterInfo
+#' 
+#' getSiteInfo
+#' 
+#' getStressorList
+#' 
+#' getStressorSpecificRegressions
+#' 
+#' SSD
+#'
+#' @docType package
+#' @name CASTfxn
+NULL
