f7ea138d2b287544cb96eb4c053ca1e791a2d856
leppott <Erik.Leppo@tetratech.com>
2019-02-01 13:12:21 -0500

    v0.1.0.9143
    Added Shiny example and function to launch, Issue #30

---
+++ R/runShinyExample.R
@@ -0,0 +1,30 @@
+#' @title run Shiny Example
+#'
+#' @description Launches Shiny app.
+#'
+#' @details The Shiny app based on the R package CASTfxn is included in the R package.
+#' This function launches that app.
+#'
+#' The Shiny app is online at:
+#' https://leppott.shinyapps.io/CAST/
+#' 
+#' A demo map (also Shiny) is at:
+#' https://leppott.shinyapps.io/CAST_Map/
+#'
+#' @examples
+#' \dontrun{
+#' # Run Function
+#' runShinyExample()
+#' }
+#
+#' @export
+runShinyExample <- function(){##FUNCTION.START
+  #
+  appDir <- system.file("shiny-examples", "CAST", package = "CASTfxn")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `CASTfxn`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+  #
+}##FUNCTION.END
