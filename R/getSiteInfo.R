f1e9abfb1e2036b168d94fd1f227e46e847c1bd7
leppott <Erik.Leppo@tetratech.com>
2019-05-16 13:04:17 -0400

    v0.1.0.9216
    Updates for R 3.6.0 staged install (Issue #65) and DESCRIPTION to remove unused packages (Issue #69).

--- R/getSiteInfo.R
+++ R/getSiteInfo.R
@@ -336,12 +336,15 @@ getSiteInfo <- function(TargetSiteID, dir_results = file.path(getwd(), "Results"
   report_format <- "html"
   #dir_rmd <- file.path(system.file(package = "CASTfxn"), "rmd")
   #strFile_RMD <- file.path(dir_rmd, "Map_Leaflet.rmd")
-  strFile_RMD <- file.path(file.path(system.file(package = "CASTfxn"), "rmd"), "Map_Leaflet.rmd")
+  #strFile_RMD <- file.path(file.path(system.file(package = "CASTfxn"), "rmd"), "Map_Leaflet.rmd")
   strFile_out_ext <- paste0(".", report_format)
   strFile_out <- paste0(TargetSiteID,".map.leaflet", strFile_out_ext)
   dir_map <- file.path(dir_results, TargetSiteID, dir.sub3)
-  rmarkdown::render(strFile_RMD, output_format=paste0(report_format,"_document"), output_file=strFile_out
-                    , output_dir=dir_map, quiet=TRUE)
+  rmarkdown::render(file.path(file.path(system.file(package = "CASTfxn"), "rmd"), "Map_Leaflet.rmd")
+                    , output_format=paste0(report_format,"_document")
+                    , output_file=strFile_out
+                    , output_dir=dir_map
+                    , quiet=TRUE)
   # place after static map so can insert
   
   #
