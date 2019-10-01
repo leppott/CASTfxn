0053d59868ebe787716ac6558c32e92ae8962c0f
ALincolnTt <Ann.Lincoln@TetraTech.com>
2019-10-01 02:38:41 -0400

    v0.1.0.9237
    Altered code to
    - rely on df_coOccur (which uses lag days)
    - be independent of biocomm
    - incorporate a merged stressor file (measured + modeled data)

--- R/getDataSets.R
+++ R/getDataSets.R
@@ -26,183 +26,84 @@
 #' @export
 getDataSets <- function(TargetSiteID
                         , compSites
-                        , stresstype = "meas"
-                        , df_stress
-                        , df_stressinfo
+                        , df_coOccur = data_bmiCoOccur
+                        , measParams = measParams
+                        , modelParams = modelParams
                         , biocomm = "bmi"
-                        , df_biometrics
-                        , lagdays = 0) {
+                        , bmiIndex = "CSCI"
+                        , df_biometrics = data_bmiMetrics
+                        , df_stressinfo = data_stressInfo) {
 
     # For QC purposes
     # TargetSiteID = TargetSiteID
     # compSites = list.CompSites$comp.sites
-    # stresstype = "meas"
-    # df_stress = data.chem.raw
-    # df_stressinfo = data.chem.info
-    # OR
-    # stresstype = "mod"
-    # df_stress = data.model.raw
-    # df_stressinfo = data.model.info
-    # AND
+    # df_coOccur = data_bmiCoOccur
+    # measParams = measParams
+    # modelParams = modelParams
     # biocomm = "bmi"
-    # df_biometrics = data.bmi.metrics
-    # lagdays = 90
-    # OR
-    # biocomm = "alg"
-    # df_biometrics = data.alg.metrics
-    # lagdays = 0
-    
+    # bmiIndex = "CSCI"
+    # df_biometrics = data_bmiMetrics
+    # df_stressinfo = data_stressInfo
+
     # Define pipe
     `%>%` <- dplyr::`%>%`
+    not_all_na <- function(x) {!all(is.na(x))}
     
-    # Get vector of parameters detected at target site
-    detect.site <- df_stress %>%
-        dplyr::filter(StationID_Master == TargetSiteID
-               , !is.na(ResultValue)) %>%
-        dplyr::select(StdParamName)
-    detect.site <- as.vector(detect.site$StdParamName)
+    biocomm <- tolower(biocomm)
     
-    # Convert data to wide format
-    if (stresstype == "meas") {
-        all.stress <- df_stress %>%
-            dplyr::select(StationID_Master, ChemSampleID, SampleDate, StdParamName
-                   , ResultValue) %>%
-            dplyr::filter(StdParamName %in% detect.site) %>%
-            tidyr::spread(key = StdParamName, value = ResultValue)
-        cl.stress <- all.stress[all.stress$StationID_Master %in% compSites,]
-        site.stress <- all.stress[all.stress$StationID_Master == TargetSiteID,]
-        
-        all.str.core <- all.stress[,c("StationID_Master", "ChemSampleID"
-                                      , "SampleDate")]
-        
-        # Get bmi matches if bmi are specified
-        if (biocomm == "bmi") {
-            bio.metr.core <- df_biometrics %>%
-                dplyr::select(StationID_Master, BMI.Metrics.SampID, Quality, SampDate) %>%
-                dplyr::mutate(LagDate = SampDate - lagdays)
-            all.match.str <- fuzzyjoin::fuzzy_right_join(all.str.core, bio.metr.core
-                                              , by = c("StationID_Master" = "StationID_Master"
-                                                       , "SampleDate" = "SampDate"
-                                                       , "SampleDate" = "LagDate")
-                                              , match_fun = list(`==`, `<=`, `>=`)) %>%
-                dplyr::filter(!is.na(StationID_Master.x)) %>%
-                dplyr::rename(StationID_Master = StationID_Master.y)
-            all.match.str <- all.match.str[,c("StationID_Master", "ChemSampleID"
-                                              , "SampleDate", "BMI.Metrics.SampID"
-                                              , "SampDate", "LagDate", "Quality")]
-            all.b.str <- unique(merge(all.match.str, all.stress
-                               , by.x = c("StationID_Master", "ChemSampleID"
-                                          , "SampleDate")
-                               , by.y = c("StationID_Master", "ChemSampleID"
-                                          , "SampleDate")
-                               , all = TRUE))
-            all.b.str <- all.b.str[!is.na(all.b.str$BMI.Metrics.SampID),]
-            cl.b.str <- all.b.str[all.b.str$StationID_Master %in% compSites,]
-            site.b.str <- all.b.str[all.b.str$StationID_Master == TargetSiteID,]
-            
-            all.b.rsp <- unique(merge(all.match.str, df_biometrics
-                               , by.x = c("StationID_Master", "BMI.Metrics.SampID"
-                                          , "SampDate", "Quality")
-                               , by.y = c("StationID_Master", "BMI.Metrics.SampID"
-                                          , "SampDate", "Quality")
-                               , all = TRUE))
-            all.b.rsp <- all.b.rsp[!is.na(all.b.rsp$ChemSampleID),]
-            cl.b.rsp <- all.b.rsp[all.b.rsp$StationID_Master %in% compSites,]
-            site.b.rsp <- all.b.rsp[all.b.rsp$StationID_Master == TargetSiteID,]
-            
-            # Identify detects for which no matched biology is available
-            nomatch.b.gap <- dplyr::setdiff(colnames(site.b.str), colnames(site.b.str))
-            
-            # Identify comparator samples
-            cl.samps <- cl.b.str[,c("StationID_Master", "ChemSampleID")]
-            cl.samps <- cl.samps[cl.samps$StationID_Master %in% compSites,]
-        }
-        # Get algal matches, if algae are specified
-        if (biocomm == "alg") {
-            # Stuff goes here
-        }  
-        
-    } else if (stresstype == "mod") {
-        
-        # If stresstype = mod, then lagdays must be zero, as date is not used
-        lagdays = 0
-            
-        all.stress <- df_stress %>%
-            dplyr::select(StationID_Master, ChemSampleID, StdParamName, ResultValue) %>%
-            dplyr::filter(StdParamName %in% detect.site) %>%
-            tidyr::spread(key = StdParamName, value = ResultValue)
-        cl.stress <- all.stress[all.stress$StationID_Master %in% compSites,]
-        site.stress <- all.stress[all.stress$StationID_Master == TargetSiteID,]
-        
-        all.str.core <- all.stress[,c("StationID_Master", "ChemSampleID")]
+    # Get datafrane of parameters detected at target site (meas & mod)
+    df_detects <- df_coOccur %>% dplyr::select_if(not_all_na)
+    useCols <- colnames(df_detects) %in% c("StationID_Master", "StressSampID"
+                                           , "StressSampDate", "RespSampID"
+                                           , "RespSampDate", modelParams
+                                           , measParams)
 
-        # Get bmi matches if bmi are specified
-        if (biocomm == "bmi") {
-            bio.metr.core <- df_biometrics %>%
-                dplyr::select(StationID_Master, BMI.Metrics.SampID, Quality, SampDate) %>%
-                dplyr::mutate(LagDate = SampDate - lagdays)
-            all.match.str <- merge(all.str.core, bio.metr.core
-                                   , by.x = "StationID_Master"
-                                   , by.y = "StationID_Master"
-                                   , all = TRUE)
-            all.match.str <- unique(all.match.str)
-            all.match.str <- all.match.str %>%
-                dplyr::mutate(SampleDate = SampDate) %>%
-                dplyr::select(StationID_Master, ChemSampleID, SampleDate
-                       , BMI.Metrics.SampID, SampDate, LagDate, Quality)
-            all.b.str <- unique(merge(all.match.str, all.stress
-                               , by.x = c("StationID_Master", "ChemSampleID")
-                               , by.y = c("StationID_Master", "ChemSampleID")
-                               , all = TRUE))
-            all.b.str <- all.b.str[!is.na(all.b.str$BMI.Metrics.SampID),]
-            cl.b.str <- all.b.str[all.b.str$StationID_Master %in% compSites,]
-            site.b.str <- all.b.str[all.b.str$StationID_Master == TargetSiteID,]
-            
-            all.b.rsp <- unique(merge(all.match.str, df_biometrics
-                               , by.x = c("StationID_Master", "BMI.Metrics.SampID"
-                                          , "SampDate", "Quality")
-                               , by.y = c("StationID_Master", "BMI.Metrics.SampID"
-                                          , "SampDate", "Quality")
-                               , all = TRUE))
-            all.b.rsp <- all.b.rsp[!is.na(all.b.rsp$ChemSampleID),]
-            cl.b.rsp <- all.b.rsp[all.b.rsp$StationID_Master %in% compSites,]
-            site.b.rsp <- all.b.rsp[all.b.rsp$StationID_Master == TargetSiteID,]
-
-            # Identify detects for which no matched biology is available
-            nomatch.b.gap <- setdiff(colnames(site.b.str), colnames(site.b.str))
-            
-            # Identify comparator samples
-            cl.samps <- cl.b.str[,c("StationID_Master", "ChemSampleID")]
-            cl.samps <- cl.samps[cl.samps$StationID_Master %in% compSites,]
-        }
-        # Get algal matches, if algae are specified
-        if (biocomm == "alg") {
-            # Stuff goes here
-        }
-        
-    }
+    siteBioStressData <- df_detects[,useCols] %>%
+        dplyr::filter(StationID_Master == TargetSiteID) %>%
+        dplyr::select_if(not_all_na)
+    useColsDetects <- colnames(siteBioStressData)
+    allBioStressData <- df_detects[,useColsDetects]
+    compBioStressData <- df_detects[,useColsDetects] %>%
+        dplyr::filter(StationID_Master %in% compSites)
     
+    df_core <- dplyr::select(df_detects, StationID_Master, StressSampID
+                            , StressSampDate, RespSampID, RespSampDate)
+    useParams <- colnames(dplyr::select(df_detects, -StationID_Master
+                                        , -StressSampID, -StressSampDate
+                                        , -RespSampID, -RespSampDate, -clust
+                                        , -BioComm, -Quality, -CSCI))
+    
+    allBioRespData <- merge(df_core, df_biometrics
+                            , by.x = c("StationID_Master", "RespSampID", "RespSampDate")
+                            , by.y = c("StationID_Master", "BMISampID", "BMISampDate"))
+    compBioRespData <- allBioRespData[allBioRespData$StationID_Master %in% compSites,]
+    siteBioRespData <- allBioRespData[allBioRespData$StationID_Master==TargetSiteID,]
+
+    # Identify "no match" data
+    # Do this when generating coOccurrence dataset
+    
+    # Subset stressor table for just detected stressors at target site
     df_stressinfo <- df_stressinfo %>% 
-        dplyr::rename(ConvertTo = Analyte) %>%
-        dplyr::filter(StdParamName %in% detect.site) %>%
-        dplyr::select(ConvertTo, ANALYSIS_TYPE, CHEMICAL_NAME, FinalUnit, StdParamName
-               , GroupNum, GroupName, LogTransf, SSD, SSTV, SensMin, SensMax
-               , TolMin, TolMax, UseInStressorID, DirIncStress)
-    
-    
+        dplyr::filter(StdParamName %in% useParams) %>%
+        dplyr::select(-ANALYSIS_TYPE, -CHEMICAL_NAME, -FinalUnit)
+
     # Return both unmatched and matched data as output
     if (biocomm == "bmi") {
-        mySubsets <- list(cluster.samps = cl.samps
-                          , chem.info = df_stressinfo
-                          , all.b.str = all.b.str
-                          , all.b.rsp = all.b.rsp
-                          , nomatch.b.meas = nomatch.b.gap)
+        mySubsets <- list(siteStressInfo = df_stressinfo
+                          , allBioStress_bmi = allBioStressData
+                          , compBioStress_bmi = compBioStressData
+                          , siteBioStress_bmi = siteBioStressData
+                          , allBioResp_bmi = allBioRespData
+                          , compBioResp_bmi = compBioRespData
+                          , siteBioResp_bmi = siteBioRespData)
     } else if (biocomm == "alg") {
-        mySubsets <- list(cluster.samps = cl.samps
-                          , chem.info = df_stressinfo
-                          , all.a.str = all.a.str
-                          , all.a.rsp = all.a.rsp
-                          , nomatch.a.meas = nomatch.a.gap)
+        mySubsets <- list(siteStressInfo = df_stressinfo
+                          , allBioStress_alg = allBioStressData
+                          , compBioStress_alg = compBioStressData
+                          , siteBioStress_alg = siteBioStressData
+                          , allBioResp_alg = allBioRespData
+                          , compBioResp_alg = compBioRespData
+                          , siteBioResp_alg = siteBioRespData)
     }
 
     return(mySubsets)
