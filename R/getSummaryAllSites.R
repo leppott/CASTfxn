7e24f33558be1d8d52eebb8d62d7fefda13d0dd1
ALincolnTt <Ann.Lincoln@TetraTech.com>
2020-05-29 19:24:30 -0400

    v0.1.0.9247
    Commented out the loop creating the overall stressor data file as it is no longer used anywhere.

--- R/getSummaryAllSites.R
+++ R/getSummaryAllSites.R
@@ -52,7 +52,7 @@ getSummaryAllSites <- function(biocommlist = c("bmi", "algae")
         df_sites = NULL
     }
     
-    
+    ### Start ####
     if (!dir.exists(file.path(dir_results))==TRUE) { # Check for results dir
         print("Results directory not found.")
         flush.console()
@@ -67,6 +67,8 @@ getSummaryAllSites <- function(biocommlist = c("bmi", "algae")
         for (site in (1:length(site_dirs))) { # Loop over each site
             # Get Target Site ID
             TargetSiteID <- site_dirs[site]
+            print(paste0("Evaluating ", TargetSiteID))
+            flush.console()
             for (b in (1:length(biocommlist))) { # For each biological community
                 biocomm = biocommlist[b]
                 if (biocomm=="BMI") { bioIndex = bmiIndex }
@@ -138,45 +140,47 @@ getSummaryAllSites <- function(biocommlist = c("bmi", "algae")
             }
             
             # get candidate causes path and chem value file lists (BMI only)
-            candcause_path <- file.path(dir_results, TargetSiteID, "BMI"
-                                        , "CandidateCauses")
-            cc_valuefiles <- list.files(candcause_path, pattern = "ChemValues")
+            # candcause_path <- file.path(dir_results, TargetSiteID, "BMI"
+            #                             , "CandidateCauses")
+            # cc_valuefiles <- list.files(candcause_path, pattern = "ChemValues")
 
             # Transpose, add biocomm, TargetSiteID to chemvalues
-            if (length(cc_valuefiles)==0) {
-                print(paste0("No stressor values file found for "
-                             , TargetSiteID, " for ", biocomm, "."))
-                flush.console()
-                next()
-            } else {
-                for (valfile in (1:length(cc_valuefiles))) {
-                    # Read file
-                    fnStrVals <- cc_valuefiles[valfile]
-                    fnStrVals <- file.path(candcause_path,fnStrVals)
-                    df_StrVals <- read.delim(fnStrVals, header = TRUE
-                                             , sep = "\t"
-                                             , na.strings = c("", NA))
-                    dfStrValsLong <- df_StrVals %>%
-                        dplyr::select(-IQRmethod, -SDmethod, -Outlier) %>%
-                        tidyr::gather(key = "Stressor", value = "StressorValue"
-                                      , na.rm = TRUE
-                                      , -StationID_Master, -StressSampID
-                                      , -StressSampDate) %>%
-                        dplyr::mutate(TargetSite = TargetSiteID
-                                      , BioComm = "BMI") %>%
-                        dplyr::select(TargetSite, BioComm, StationID_Master
-                                      , StressSampID, StressSampDate, Stressor
-                                      , StressorValue)
-                    
-                }
-                
-                if (site==1) {
-                    df_StrValsAll <- dfStrValsLong
-                } else {
-                    df_StrValsAll <- rbind(df_StrValsAll, dfStrValsLong)
-                }
-                
-            } # Complete compilation of stressor values
+            # if (length(cc_valuefiles)==0) {
+            #     print(paste0("No stressor values file found for "
+            #                  , TargetSiteID, " for ", biocomm, "."))
+            #     flush.console()
+            #     next()
+            # } else {
+            #     for (valfile in (1:length(cc_valuefiles))) {
+            #         # Read file
+            #         fnStrVals <- cc_valuefiles[valfile]
+            #         fnStrVals <- file.path(candcause_path,fnStrVals)
+            #         df_StrVals <- read.delim(fnStrVals, header = TRUE
+            #                                  , sep = "\t"
+            #                                  , na.strings = c("", NA))
+            #         dfStrValsLong <- df_StrVals %>%
+            #             dplyr::select(-IQRmethod, -SDmethod, -Outlier) %>%
+            #             tidyr::gather(key = "Stressor", value = "StressorValue"
+            #                           , na.rm = TRUE
+            #                           , -StationID_Master, -StressSampID
+            #                           , -StressSampDate) %>%
+            #             dplyr::mutate(TargetSite = TargetSiteID
+            #                           , BioComm = "BMI") %>%
+            #             dplyr::select(TargetSite, BioComm, StationID_Master
+            #                           , StressSampID, StressSampDate, Stressor
+            #                           , StressorValue)
+            #         
+            #     }
+            #     
+            #     if (site==1) {
+            #         df_StrValsAll <- dfStrValsLong
+            #     } else {
+            #         df_StrValsAll <- rbind(df_StrValsAll, dfStrValsLong)
+            #     }
+            #     
+            # } # Complete compilation of stressor values
+            # 
+            # cc_valuefiles <- NULL
 
         } # Finish iterate through sites in results folder
         
