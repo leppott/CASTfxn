358919e5ae15e3d28742511ff40720a082d7aa2e
Erik Leppo <Erik.Leppo@tetratech.com>
2020-08-31 15:27:07 -0400

    v0.1.0.9276
    Update version numbers and clean up of merge.

--- inst/shiny-examples/CAST_SMC/ui.R
+++ inst/shiny-examples/CAST_SMC/ui.R
@@ -23,7 +23,7 @@ shinyUI(fluidPage(
   
   # Need for console messages to Shiny
   shinyjs::useShinyjs()
-  , navbarPage("Causal Assessment Screening Tool (CAST), SMC v0.1.0.9255"
+  , navbarPage("Causal Assessment Screening Tool (CAST), SMC 2020-09-01"
   #, navbarPage(paste0("Causal Assessment Screening Tool (CAST), SMC v0.1.0.9251, Test run time = ", Sys.time())
              , theme = "bootstrap.css"
              , inverse = TRUE
