0497c33a2e5a3afce53f9a0017a928ac17a78af4
ALincolnTt <Ann.Lincoln@TetraTech.com>
2020-09-18 18:25:55 -0400

    v0.1.0.9279
    Added license terms.

--- R/getWoE.R
+++ R/getWoE.R
@@ -1,3 +1,8 @@
+#  Copyright 2020 TetraTech. All rights reserved.
+#  Use, copying, modification, or distribution of this file or any of its contents 
+#  is expressly prohibited without prior written permission of TetraTech.
+#
+#
 #' @title Weight-of-Evidence summary
 #' 
 #' @description Summarize weight of evidence using scores from other functions.
