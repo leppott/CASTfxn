562a8192aa586f2107956a67e8a77d6dc0399a8e
ALincolnTt <Ann.Lincoln@TetraTech.com>
2020-09-18 18:21:41 -0400

    v0.1.0.9260
    Added license terms.

--- R/getCoOccur.R
+++ R/getCoOccur.R
@@ -1,3 +1,8 @@
+#  Copyright 2020 TetraTech. All rights reserved.
+#  Use, copying, modification, or distribution of this file or any of its contents 
+#  is expressly prohibited without prior written permission of TetraTech.
+#
+#
 # library(dplyr)
 # library(ggplot2)
 # library(gridExtra)
