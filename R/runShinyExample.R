dc8326fdde3de4c4d5877be67eadf2fc70edc02a
ALincolnTt <Ann.Lincoln@TetraTech.com>
2020-09-18 18:26:07 -0400

    v0.1.0.9279
    Added license terms.

--- R/runShinyExample.R
+++ R/runShinyExample.R
@@ -1,3 +1,8 @@
+#  Copyright 2020 TetraTech. All rights reserved.
+#  Use, copying, modification, or distribution of this file or any of its contents 
+#  is expressly prohibited without prior written permission of TetraTech.
+#
+#
 #' @title run Shiny Example
 #'
 #' @description Launches Shiny app.
