904a0a73554c5d74e76f0554d948afd96cac2829
ALincolnTt <Ann.Lincoln@TetraTech.com>
2020-09-18 18:24:58 -0400

    v0.1.0.9292
    Added license terms.

--- R/getSummaryAllSites.R
+++ R/getSummaryAllSites.R
@@ -1,3 +1,8 @@
+#  Copyright 2020 TetraTech. All rights reserved.
+#  Use, copying, modification, or distribution of this file or any of its contents 
+#  is expressly prohibited without prior written permission of TetraTech.
+#
+#
 #' @title Get summary of all sites in results directory
 #' 
 #' @description Pull relevant information from the final Weight of Evidence
