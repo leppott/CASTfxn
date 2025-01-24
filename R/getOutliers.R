7ee6369947b263f28b32bd78340310bad9a2856c
ALincolnTt <Ann.Lincoln@TetraTech.com>
2025-01-24 16:39:36 -0500

    v0.2.0.9007
    Simplify function.

--- R/getOutliers.R
+++ R/getOutliers.R
@@ -54,16 +54,16 @@ getOutliers <- function(df_data, df_meta, dir_plots) {##FUNCTION.START
     dplyr::summarise(LogTransf = max(LogTransf), .groups = "drop_last")
 
   # Merge data with metadata (note that some metadata may not be in the raw data)
-  df_data <- merge(df_data, df_meta[, c("StdParamName", "LogTransf")]
-                   , by.x = "StdParamName", by.y = "StdParamName"
-                   , all.x = TRUE)
+  df_data <- merge(df_data, df_meta[, c("StdParamName", "LogTransf")],
+                   by.x = "StdParamName", by.y = "StdParamName",
+                   all.x = TRUE)
   df_data <- df_data %>%
-    dplyr::mutate(ResultValue = ifelse(ResultValue < 0 & LogTransf == 1
-                                       , NA, ResultValue)) %>%
+    dplyr::mutate(ResultValue = ifelse(ResultValue < 0 & LogTransf == 1,
+                                       NA, ResultValue)) %>%
     dplyr::filter(!is.na(ResultValue)) %>%
-    dplyr::mutate(TransfResult = ifelse(LogTransf == 1
-                                        , suppressWarnings(log1p(ResultValue))
-                                        , ResultValue))
+    dplyr::mutate(TransfResult = ifelse(LogTransf == 1,
+                                        suppressWarnings(log1p(ResultValue)),
+                                        ResultValue))
 
   params <- unique(as.character(df_data$StdParamName))
 
@@ -75,51 +75,8 @@ getOutliers <- function(df_data, df_meta, dir_plots) {##FUNCTION.START
 
     df_sub <- df_data %>%  # Subset for just one parameter at a time
       dplyr::filter(StdParamName == paramName) %>%
-      dplyr::select(StationID, StressSampleID, StressSampleDate
-                    , StdParamName, LogTransf, ResultValue, TransfResult)
-
-    # Test transformed variables
-    # df_sub1 <- df_data %>%  # Subset for just one parameter at a time
-    #   dplyr::filter(StdParamName == paramName) %>%
-    #   dplyr::select(StationID, StressSampleID, StressSampleDate
-    #                 , StdParamName, LogTransf, ResultValue)
-    #
-    # # Need to change definition of LogTransf to are the data for the StdParamName already log transformed?
-    # if (min(df_sub1$ResultValue) < 0 | !(paramName %in% c("pH", "DO_mg_L"))) {
-    #   fnorm_orig <- fitdistrplus::fitdist(df_sub1$ResultValue, "norm")
-    #   flnorm_orig <- fitdistrplus::fitdist(df_sub1$ResultValue, "lnorm")
-    #   df_sub1_gofstat <- fitdistrplus::gofstat(list(fnorm_orig, flnorm_orig))
-    #   df_sub <- df_sub %>%
-    #     dplyr::mutate(BestFitDistr = ifelse(df_sub1_gofstat$aic[1] < df_sub1_gofstat$aic[2]
-    #                                         , "normal", "lognormal")
-    #                   , UseVal = ifelse(BestFitDistr == "normal", ResultValue, TransfResult))
-    #   fitdistrplus::cdfcomp(list(fnorm_orig, flnorm_orig), xlogscale = TRUE
-    #                         , ylogscale = TRUE, legendtext = c("normal", "lognormal"))
-    # }
-    #
-    # df_sub2 <- df_data %>%
-    #   dplyr::filter(StdParamName == paramName) %>%
-    #   dplyr::select(StationID, StressSampleID, StressSampleDate
-    #                 , StdParamName, LogTransf, TransfResult) %>%
-    #   dplyr::mutate(BestFitDistr = ifelse(LogTransf == 1, "lognormal", "normal")
-    #                 , UseVal = ifelse(BestFitDistr == "normal", ResultValue, TransfResult))
-    # fnorm_transf <- fitdistrplus::fitdist(df_sub2$TransfResult, "norm")
-    # flnorm_transf <- fitdistrplus::fitdist(df_sub2$TransfResult, "lnorm")
-    # df_sub2_gofstat <- fitdistrplus::gofstat(list(fnorm_transf, flnorm_transf))
-    # fitdistrplus::cdfcomp(list(fnorm_transf, flnorm_transf), xlogscale = TRUE
-    #                               , ylogscale = TRUE, legendtext = c("normal", "lognormal"))
-
-    ## Transform/scale variables ----
-    ## Plot histograms before/after ----
-    # cols <- setdiff(colnames(WS.STATE.final), c("COMID", "US_L3CODE"))
-    # df.temp <- df_sub[, c("COMID", "US_L3CODE")]
-    # df.temp_scaled <- df_sub[, c("COMID", "US_L3CODE")]
-    # df.lambda <- data.frame(Variable = character(), Lambda = double())
-    # for (c in seq_along(cols)) {
-    #   col <- cols[c]
-    #   print(col)
-
-
+      dplyr::select(StationID, StressSampleID, StressSampleDate,
+                    StdParamName, LogTransf, ResultValue, TransfResult)
 
     fn <- paste0(paramName, ".png")
     fn2 <- paste0(paramName, "_transf.png")
@@ -141,37 +98,24 @@ getOutliers <- function(df_data, df_meta, dir_plots) {##FUNCTION.START
         ggplot2::theme_bw()
       ggplot2::ggsave(file.path(dir_plots, fn2), p2, width = 6, height = 4
                       , units = "in")
-      fnorm_transf <- fitdistrplus::fitdist(df_sub$TransfResult, "norm")
-      flnorm_transf <- fitdistrplus::fitdist(df_sub$TransfResult, "lnorm")
-      df_sub2_gofstat <- fitdistrplus::gofstat(list(fnorm_transf, flnorm_transf))
-      fitdistrplus::cdfcomp(list(fnorm_transf, flnorm_transf), xlogscale = FALSE
-                                    , ylogscale = FALSE, legendtext = c("normal", "lognormal"))
-      fitdistrplus::denscomp(list(fnorm_transf, flnorm_transf), xlogscale = FALSE
-                            , ylogscale = FALSE, legendtext = c("normal", "lognormal"))
-      fitdistrplus::qqcomp(list(fnorm_transf, flnorm_transf), xlogscale = FALSE
-                            , ylogscale = FALSE, legendtext = c("normal", "lognormal"))
-      fitdistrplus::ppcomp(list(fnorm_transf, flnorm_transf), xlogscale = FALSE
-                            , ylogscale = FALSE, legendtext = c("normal", "lognormal"))
     }
 
     # 3*IQR method for identifying outliers
-    iqr <- stats::quantile(df_sub$TransfResult, probs = c(0.25, 0.75)
-                           , na.rm = TRUE)
+    iqr <- stats::quantile(df_sub$TransfResult, probs = c(0.25, 0.75), na.rm = TRUE)
     iqr1.5 <- 1.5 * stats::IQR(df_sub$TransfResult, na.rm = TRUE)
     outlowlim <- iqr[1] - iqr1.5
     outhilim <- iqr[2] + iqr1.5
 
     df_sub <- df_sub %>%
-      dplyr::mutate(IQRmethod = ifelse((TransfResult < outlowlim)
-                                       , "Outlier low"
-                                       , ifelse((TransfResult > outhilim)
-                                                , "Outlier high", "Good")))
+      dplyr::mutate(IQRmethod = dplyr::case_when(TransfResult < outlowlim ~ "Outlier low",
+                                                 TransfResult > outhilim ~ "Outlier high",
+                                                 TRUE ~ "Good"))
 
     # 6*sd method for identifying outliers
-    paramMean <- mean(df_sub$TransfResult[is.finite(df_sub$TransfResult)]
-                      , na.rm = TRUE)
-    paramSD <- stats::sd(df_sub$TransfResult[is.finite(df_sub$TransfResult)]
-                         , na.rm = TRUE)
+    paramMean <- mean(df_sub$TransfResult[is.finite(df_sub$TransfResult)],
+                      na.rm = TRUE)
+    paramSD <- stats::sd(df_sub$TransfResult[is.finite(df_sub$TransfResult)],
+                         na.rm = TRUE)
     df_sub <- df_sub %>%
       dplyr::mutate(SDmethod = ifelse((abs(TransfResult - paramMean) >
                                          (6 * paramSD)), "Outlier", "Good"))
@@ -181,12 +125,12 @@ getOutliers <- function(df_data, df_meta, dir_plots) {##FUNCTION.START
     # If either IQR method OR SD method aren't evaluated, then mark as "NE"
     # If either IQR method or SD method suggest an outlier, then mark as "Outlier"
     df_sub <- df_sub %>%
-      dplyr::mutate(IQRmethod = ifelse(!is.na(IQRmethod), IQRmethod, "NE")
-                    , SDmethod = ifelse(!is.na(SDmethod), SDmethod, "NE")) %>%
-      dplyr::mutate(Outlier = dplyr::case_when((IQRmethod == "Good") & (SDmethod == "Good") ~ "Good"
-                                               , (IQRmethod == "NE") | (SDmethod == "NE") ~ "NE"
-                                               , grepl("Outlier", IQRmethod) & (SDmethod == "Outlier") ~ "Outlier"
-                                               , TRUE ~ "Good"))
+      dplyr::mutate(IQRmethod = ifelse(!is.na(IQRmethod), IQRmethod, "NE"),
+                    SDmethod = ifelse(!is.na(SDmethod), SDmethod, "NE")) %>%
+      dplyr::mutate(Outlier = dplyr::case_when((IQRmethod == "Good") & (SDmethod == "Good") ~ "Good",
+                                               (IQRmethod == "NE") | (SDmethod == "NE") ~ "NE",
+                                               grepl("Outlier", IQRmethod) & (SDmethod == "Outlier") ~ "Outlier",
+                                               TRUE ~ "Good"))
 
     if (a == 1) {
       df_temp <- df_sub
@@ -195,8 +139,8 @@ getOutliers <- function(df_data, df_meta, dir_plots) {##FUNCTION.START
     }
   } # End parameter iteration
 
-  df_temp <- dplyr::select(df_temp, StressSampleID, StdParamName, ResultValue
-                           , TransfResult, LogTransf, IQRmethod, SDmethod, Outlier)
+  df_temp <- dplyr::select(df_temp, StressSampleID, StdParamName, ResultValue,
+                           TransfResult, LogTransf, IQRmethod, SDmethod, Outlier)
 
   # Return what?
   myOutliers <- df_temp
