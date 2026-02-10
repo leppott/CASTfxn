e2d0ca15d5a4cb6a836dc4950a1c640eb815fb15
Laura Naslund <Naslund.Laura@epa.gov>
2026-02-10 11:56:28 -0500

    Replace TargetSiteID punctuation with _ as in all other prep functions.

--- inst/shiny-examples/CASTool/CASTool.r
+++ inst/shiny-examples/CASTool/CASTool.r
@@ -668,7 +668,8 @@ if (boo_Shiny == TRUE) {
   message(paste(prog_msg, prog_det, sep = "; "))
 }## IF ~ boo_Shiny ~ END
 #
-df_targets <- readRDS(file.path(out.dir, dn_checked_sk, "df_targets.rds"))
+df_targets <- readRDS(file.path(out.dir, dn_checked_sk, "df_targets.rds")) |>
+  mutate(TargetSiteID = stringr::str_replace_all(TargetSiteID, "[:punct:]", "_"))
 
 ### Evaluate each target site
 ## Use this for debugging
